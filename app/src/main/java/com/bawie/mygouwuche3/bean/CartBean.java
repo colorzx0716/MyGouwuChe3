3451baf42c270ca1a8adf8f00e2ed390736100ce
colorzx0716 <31384907+colorzx0716@users.noreply.github.com>
2017-10-21 16:40:51 +0800

    Initial commit

---
+++ app/src/main/java/com/bawie/mygouwuche3/bean/CartBean.java
@@ -0,0 +1,13 @@
+package com.bawie.mygouwuche3.bean;
+
+/**
+ * Created by 张肖肖 on 2017/10/20.
+ */
+
+public class CartBean {
+
+    /** 商品ID */
+    private String goodsid;
+    /** 店铺ID */
+    private String storeid;
+}
